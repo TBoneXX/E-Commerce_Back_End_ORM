431af3ca2404ecea485603b53f12eb70d2b39274
Mark Turner <mark.edward.turner@gmail.com>
2022-12-15 17:01:11 -0500

    Build tag route api

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -3,10 +3,22 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
-// get all products
+// get all products including associated Category and Tag data
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+    Product.findAll({
+        attributes: ['id', 'category_name'],
+        include: [ 
+          {
+            model: Tag,
+            attributes: ['id', 'tag_name']
+          }
+        ]
+      })
+      .then(categoryData => res.json(categoryData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
 });
 
 // get one product
